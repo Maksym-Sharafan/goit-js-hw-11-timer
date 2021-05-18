4ae584961992a20ff7a70cce5dd30acf7b873876
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-05-18 07:56:32 +0300

    done

---
+++ index.js
@@ -0,0 +1,62 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+}
+
+class CountdownTimer {
+  
+  constructor({targetDate, selector}) {
+    this.intervalId = null;    
+    this.targetDate = targetDate;
+    this.selector = selector;
+  }
+  
+  start() {  
+     const finishDate = this.targetDate;  
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = finishDate - currentTime;
+
+      if (deltaTime <= 0) {
+        return;
+      }
+
+      const time = this.getTimeComponents(deltaTime);
+
+      this.updateClockFace(time);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  getTimeComponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+     return { days, hours, mins, secs };
+  }
+
+  updateClockFace({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+  }
+  
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 28, 2022'),
+});
+
+timer.start();
+
+document.addEventListener('click', timer.stop.bind(timer));
+document.addEventListener('keydown', timer.start.bind(timer));
